c6c976f48d49fa181153ad77d7f9e686f911c8e7
Vikash Chauhan <vikashchauhan51@gmail.com>
2022-06-10 15:56:04 +0530

    added more fields

--- SonarRulesVerfication/Foo.cs
+++ SonarRulesVerfication/Foo.cs
@@ -6,7 +6,10 @@ public class Foo
 {
     private string name = "foobar"; // Noncompliant
 
+    private int age = 20; // Noncompliant
+    private bool status = true;// Noncompliant
     public string DefaultName { get; } = "foobar"; // Noncompliant
+
 
     public Foo(string value = "foobar") // Noncompliant
     {
