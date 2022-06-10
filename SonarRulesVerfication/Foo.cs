9bdf9f3d56cba87556ab191ce279284bab6e6195
Vikash Chauhan <vikashchauhan51@gmail.com>
2022-06-10 15:47:55 +0530

    Added foo class

---
+++ SonarRulesVerfication/Foo.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace SonarRulesVerfication;
+
+public class Foo
+{
+    private string name = "foobar"; // Noncompliant
+
+    public string DefaultName { get; } = "foobar"; // Noncompliant
+
+    public Foo(string value = "foobar") // Noncompliant
+    {
+        var something = value ?? "foobar"; // Noncompliant
+    }
+}
