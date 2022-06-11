66b7db2897f94e40dddf73205f5ae2565704f66b
Vikash Chauhan <vikashchauhan51@gmail.com>
2022-06-11 16:23:05 +0530

    Unnecessary Boolean literal(s).

--- SonarRulesVerfication/Foo.cs
+++ SonarRulesVerfication/Foo.cs
@@ -4,22 +4,17 @@ namespace SonarRulesVerfication;
 
 public class Foo
 {
-    private string name = "foobar"; // Noncompliant
 
-    public string DefaultName { get; } = "foobar"; // Noncompliant
-
-
-
-    public Foo(string value = "foobar") // Noncompliant
+    public Foo()
     {
-        var something = value ?? "foobar"; // Noncompliant
 
-        if (booleanMethod() == true) { Console.WriteLine(something); }
-        if (booleanMethod() == false) { /* ... */ }
-       
+
+        if (BooleanMethod() == true) { Console.WriteLine("Hello!"); } // Noncompliant
+        if (BooleanMethod() == false) { /* ... */ } // Noncompliant
+
     }
 
-    bool booleanMethod()
+    static bool BooleanMethod()
     {
         return false;
     }
