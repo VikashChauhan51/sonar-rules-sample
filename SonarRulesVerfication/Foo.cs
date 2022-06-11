c48cdb6bf5ad339bf773d427f80e188569fcff3f
Vikash Chauhan <vikashchauhan51@gmail.com>
2022-06-11 15:39:40 +0530

    added boolean

--- SonarRulesVerfication/Foo.cs
+++ SonarRulesVerfication/Foo.cs
@@ -8,8 +8,19 @@ public class Foo
 
     public string DefaultName { get; } = "foobar"; // Noncompliant
 
+
+
     public Foo(string value = "foobar") // Noncompliant
     {
         var something = value ?? "foobar"; // Noncompliant
+
+        if (booleanMethod() == true) { Console.WriteLine(something); }
+        if (booleanMethod() == false) { /* ... */ }
+       
+    }
+
+    bool booleanMethod()
+    {
+        return false;
     }
 }
