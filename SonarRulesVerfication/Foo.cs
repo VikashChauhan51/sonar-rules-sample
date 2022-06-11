ee240838e451829947ad92907c4ff9959b0d04f4
Vikash Chauhan <vikashchauhan51@gmail.com>
2022-06-11 14:27:04 +0530

    foo updated

--- SonarRulesVerfication/Foo.cs
+++ SonarRulesVerfication/Foo.cs
@@ -4,13 +4,14 @@ namespace SonarRulesVerfication;
 
 public class Foo
 {
-    private const string Foobar = "foobar";
+    private const string Hello = "Hello word constant";
 
-    public string DefaultName { get; } = Foobar;
+    public string DefaultName { get; } = "Hello word constant";
 
 
-    public Foo(string value = Foobar) 
+    public Foo(string value = "Hello word constant") 
     {
-        var something = value ?? Foobar; 
+        var something = value ?? Hello;
+        Console.WriteLine(something);
     }
 }
